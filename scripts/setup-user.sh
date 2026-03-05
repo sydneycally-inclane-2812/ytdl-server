6236f765070e64b86d7c8262853ab995a9218ff6
Dat's child No.2 <tranhungdat1210@gmail.com>
2026-03-05 03:54:05 +0000

    fix: update user creation id to be somewhat unique

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 # Create users and group if they don't exist
 if ! id -u ytdl-uvicorn >/dev/null 2>&1; then
-	useradd -r -s /bin/false ytdl-uvicorn
+	useradd -u 3636 -r -s /bin/false ytdl-uvicorn
 fi
 if ! id -u ytdl-celery >/dev/null 2>&1; then
-	useradd -r -s /bin/false ytdl-celery
+	useradd -u 3637 -r -s /bin/false ytdl-celery
 fi
 if ! getent group ytdl >/dev/null 2>&1; then
 	groupadd ytdl
 fi
 usermod -aG ytdl ytdl-uvicorn
 usermod -aG ytdl ytdl-celery
-echo "ytdl-uvicorn and ytdl-celery users exists and added to ytdl group."
+echo "ytdl-uvicorn and ytdl-celery users exist and have been added to the ytdl group."
